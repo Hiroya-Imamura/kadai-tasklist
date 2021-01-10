1b51c314930e51023ba931cb823c7f9c508ebaf7
hiroya.imamura <hrimamura26@gmail.com>
2021-01-10 07:55:59 +0000

    login

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,32 +3,39 @@
 @section('content')
 
     <!-- ここにページ毎のコンテンツを書く -->
-    <h1>タスク一覧</h1>
-
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>タスク</th>
-                    <th>状態</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task2)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task2->id, ['task' => $task2->id]) !!}</td>
-                    <td>{{ $task2->content }}</td>
-                    <td>{{ $task2->status }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
+    @if(Auth::check())
+        <h1>{{ Auth::user()->name }}のタスク一覧</h1>
     
-    {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
-    {{-- ユーザ登録ページへのリンク --}}
-    {!! link_to_route('signup.get', '今すぐ登録！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>タスク</th>
+                        <th>状態</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task2)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task2->id, ['task' => $task2->id]) !!}</td>
+                        <td>{{ $task2->content }}</td>
+                        <td>{{ $task2->status }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+        
+        {{-- メッセージ作成ページへのリンク --}}
+        {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
+    @else
+        <h1>タスク一覧サイトへようこそ</h1>
+        
+        {{-- ユーザ登録ページへのリンク --}}
+        {!! link_to_route('signup.get', '今すぐ登録！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+    @endif
+
+
 
 @endsection
